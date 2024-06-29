18c7cd3c02c18e4c5f9373bda6786a2f11ecda81
Alex Erofeev <xnet141@gmail.com>
2024-06-29 12:10:34 +0300

    Add textarea to /details/ ...

--- app.rb
+++ app.rb
@@ -50,8 +50,10 @@ end
 
 get '/details/:post_id' do
 	post_id = params[:post_id]
-	@results = @db.execute 'select * from Posts order by id = ?', [post_id]
 
-	erb "Displaying information for post with id #{post_id}"
+	@results = @db.execute 'select * from Posts order by id = ?', [post_id]
+	@row = @results[0]
+
+	erb :details
 end
 
