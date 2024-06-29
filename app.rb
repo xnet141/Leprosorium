a1cb4e2e4290f77f8f00fda70590ed1826c60188
Alex Erofeev <xnet141@gmail.com>
2024-06-29 04:55:52 +0300

    Add stub for new

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 #encoding: utf-8
 require 'rubygems'
 require 'sinatra'
-require 'sinatra/reloader'
+require 'sinatra/reloader' #автоперезапуск
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+get '/new' do
+  erb "Hello World"
+end
+	
