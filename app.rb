d13065a5d95b98aa66b3bd962427c713d1c6448e
Alex Erofeev <xnet141@gmail.com>
2024-06-29 12:18:14 +0300

    Add simple /details/ ... post

--- app.rb
+++ app.rb
@@ -57,3 +57,12 @@ get '/details/:post_id' do
 	erb :details
 end
 
+post '/details/:post_id' do
+	post_id = params[:post_id]
+	content = params[:content]
+
+	erb "You typed comment #{content} for post #{post_id}"
+
+end
+
+
